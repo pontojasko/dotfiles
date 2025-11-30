57cccf8aad092e3a39100dafc358ed2f3928aa38
heitor <pontojasko>
2025-11-30 15:49:13 -0300

    brightnesnotificationsandtoomuchthingstotthink

--- .config/eww/scripts/toggle-player.sh
+++ .config/eww/scripts/toggle-player.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-if eww active-windows | grep -q "music_player"; then
+if eww active-windows | grep -q "spotify_control"; then
+    # SE ESTIVER ABERTA: Fecha e libera o teclado (reset)
     eww close spotify_control
+    hyprctl dispatch submap reset
 else
+    # SE ESTIVER FECHADA: Abre e trava o teclado no modo (submap)
     eww open spotify_control
+    hyprctl dispatch submap spotify_menu
 fi
