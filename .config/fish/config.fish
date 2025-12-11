b44572bd51ca66aeedd74f2ab28e2cc4a3fbb3ad
heitor <pontojasko>
2025-12-11 19:56:11 -0300

    modoclaro70

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,3 +2,6 @@ if status is-interactive
 	neofetch
     # Commands to run in interactive sessions can go here
 end
+
+# Created by `pipx` on 2025-12-11 19:19:59
+set PATH $PATH /home/jasko/.local/bin
