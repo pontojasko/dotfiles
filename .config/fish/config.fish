d4e8a2ca55429974cf95ebaf047aac4ceefd6d1d
heitor <pontojasko>
2025-12-21 06:15:18 -0300

    screen recorder

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -3,5 +3,13 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 
+# Aliases e Abbrs
+    abbr -a gst 'git status'
+    abbr -a gp 'git push'
+
 # Created by `pipx` on 2025-12-11 19:19:59
 set PATH $PATH /home/jasko/.local/bin
+set -g fish_greeting ""
+starship init fish | source
+zoxide init fish | source # (Opcional: o zoxide é um 'cd' inteligente que aprende seus caminhos)
+end
--- .config/hypr/scripts/playerctl.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-#todo: send mpd song change notification to mako
-
-title=$(playerctl metadata | grep 'title'| awk '{printf "%s", "Title:";for(i=3;i<=NF;i++){printf "%s", $i};}')
-notify-send $title
-
-
