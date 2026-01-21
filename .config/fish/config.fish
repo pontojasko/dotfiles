27f37cfb81c2bbcf8c4c612179cf826fcaf99430
heitor <pontojasko>
2026-01-21 20:22:38 -0300

    tudo certinho por enquanto

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,12 +5,20 @@ end
 
 # Aliases e Abbrs
     abbr -a gst 'git status'
-    abbr -a gp 'git push'
-    abbr -a g 'git'
-    abbr -a gm 'git commit -m'
+    abbr -a push 'git push'
+    abbr -a gm --set-cursor 'git commit -m "%"'
+
     abbr -a st 'shutdown'
+
     abbr -a ai 'mods'
-	
+
+    abbr -a feat --set-cursor 'git commit -m "feat: %"'
+	abbr -a fix --set-cursor 'git commit -m "fix: %"'
+    abbr -a refactor --set-cursor 'git commit -m "refactor: %"'
+    abbr -a ui --set-cursor 'git commit -m "ui: %"'   
+    abbr -a style --set-cursor 'git commit -m "style: %"'
+    abbr -a run 'npm run dev'
+
 # Created by `pipx` on 2025-12-11 19:19:59
 set PATH $PATH /home/jasko/.local/bin
 set -g fish_greeting ""
@@ -21,4 +29,4 @@ set -gx GEMINI_API_KEY "AIzaSyCYeJzdCoLbWZwLWLP1-aCNOYI7KTmUD0s"
 
 set -gx MODS_DEFAULT_MODEL "gemini-1.5-flash"
 set -gx MODS_DEFAULT_API "google"
-
+set -gx EDITOR nano set -gx VISUAL nano
